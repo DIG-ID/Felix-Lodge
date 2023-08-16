004d870360c31bd224b6d5f16dc5e3734e958181
Daniel-Digid <daniel@dig.id>
2023-08-16 12:17:34 +0100

    dev

--- template-parts/footer-main.php
+++ template-parts/footer-main.php
@@ -2,7 +2,7 @@
 	<div class="footer-content container mx-auto py-8 grid grid-cols-1 gap-5 lg:grid-cols-10 w-full px-8 xl:px-0">
 		<div class="col-span-1 lg:col-span-3">
 		<?php 
-		$footer_logo = get_field('settings_footer_logo', 'option');
+		$footer_logo = get_field('settings_website_logo', 'option');
 		if( !empty( $footer_logo ) ): ?>
 			<img src="<?php echo esc_url($footer_logo['url']); ?>" alt="<?php echo esc_attr($footer_logo['alt']); ?>" class="max-w-full" />
 		<?php endif; ?>
@@ -10,7 +10,6 @@
 	</div>
 	<div class="footer-content container mx-auto py-8 grid gap-5 grid-cols-10 w-full px-8 xl:px-0">
 		<div class="col-span-5 lg:col-span-3">
-			<h3 class="text-text-color title-subtitle"><?php esc_html_e( 'Kontakt' , 'paun' ) ?></h3>
 			<p class="text-body mt-10">
 				<?php the_field( 'contact_address', 'option' ) ?>
 			</p>
@@ -21,11 +20,10 @@
 			</p>
 		</div>
 		<div class="col-span-5 lg:col-span-3">
-			<h3 class="text-text-color title-subtitle"><?php esc_html_e( 'Seiten' , 'paun' ) ?></h3>
 			<?php
 			wp_nav_menu(
 				array(
-					'theme_location' => 'footer',
+					'theme_location' => 'main',
 					'container'      => false,
 					'menu_class'     => 'menu-footer mt-10',
 					'items_wrap'     => '<ul id="%1$s" class="%2$s menu-top-level">%3$s</ul>',
@@ -46,9 +44,8 @@
 			?>
 		</div>
 		<div class="col-span-10 lg:col-span-4 pt-12 lg:pt-0">
-			<h3 class="text-text-color title-subtitle !mb-0 lg:mb-8"><?php esc_html_e( 'Newsletter' , 'paun' ) ?></h3>
 			<?php 
-			$form_shortcode = get_field('settings_newsletter_shortcode', 'option');
+			$form_shortcode = get_field('settings_newsletter_form_shortcode', 'option');
 			echo do_shortcode($form_shortcode);
 			?>
 		</div>
