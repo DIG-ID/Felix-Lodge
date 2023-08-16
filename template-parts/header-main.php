58b8ba65619dfddbe4059ad11bef5471bb1c4f05
Daniel-Digid <daniel@dig.id>
2023-08-16 11:14:44 +0100

    dev

--- template-parts/header-main.php
+++ template-parts/header-main.php
@@ -1,25 +1,34 @@
 <header id="header-main" class="header-main px-8 xl:px-0 w-full top-0 box-border bg-bg-color" itemscope itemtype="http://schema.org/WebSite">
 	<nav class="navigation-main container mx-auto" role="navigation" aria-label="<?php esc_attr_e( 'Main navigation', 'paun' ); ?>">
 		<div class="navigation-main-content flex flex-col sm:flex-row">
-			<div class="w-full mx-auto sm:w-5/12 xl:w-1/3 max-w-7xl flex sm:order-2 px-0 py-6 sm:py-8">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'main',
-					'container'      => false,
-					'menu_class'     => 'main-menu-top-level',
-					'items_wrap'     => '<ul id="%1$s" class="%2$s menu-top-level w-full justify-between sm:justify-end flex">%3$s</ul>',
-					'fallback_cb'    => '__return_false',
-				),
-			);
-			?>
-			</div>
-			<div class="header-logo-wrapper w-full mx-auto sm:w-7/12 xl:w-2/3 flex p-0 sm:py-8 sm:px-0">
-			<?php 
-			$main_logo = get_field('settings_website_logo', 'option');
-			if( !empty( $main_logo ) ): ?>
-				<a href="<?php echo get_home_url(); ?>"><img src="<?php echo esc_url($main_logo['url']); ?>" alt="<?php echo esc_attr($main_logo['alt']); ?>" class="header-logo" /></a>
-			<?php endif; ?>
+			<div class="w-full mx-auto max-w-7xl flex flex-wrap items-center justify-center px-0 py-6 sm:py-8">
+				<div class="header-logo-wrapper mb-4 sm:mb-0">
+					<?php 
+					$main_logo = get_field('settings_website_logo', 'option');
+					if (!empty($main_logo)): ?>
+						<a href="<?php echo get_home_url(); ?>">
+							<img src="<?php echo esc_url($main_logo['url']); ?>" alt="<?php echo esc_attr($main_logo['alt']); ?>" class="header-logo" />
+						</a>
+					<?php endif; ?>
+				</div>
+
+				<div class="w-full">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'main',
+							'container'      => false,
+							'menu_class'     => 'main-menu-top-level flex justify-between', // Adjust classes here
+							'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+							'fallback_cb'    => '__return_false',
+							'walker'         => new Left_Right_Menu_Walker(),
+							'before'         => '', // Add any necessary HTML before the menu item
+							'after'          => '', // Add any necessary HTML after the menu item
+						)
+					);
+					
+					?>
+				</div>
 			</div>
 		</div>
 	</nav>
