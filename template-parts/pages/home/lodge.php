be191b06c84773fc1d940fa6ec3beebe79b298e6
Daniel-Digid <daniel@dig.id>
2023-08-21 16:18:32 +0100

    dev

--- template-parts/pages/home/lodge.php
+++ template-parts/pages/home/lodge.php
@@ -1,11 +1,18 @@
-<section id="section-lodge" class="section-lodge py-2 lg:pb-32 lg:pt-0 bg-white overflow-hidden">
-    <div class="container mx-auto grid grid-cols-3">
-        <div class="col-span-2">
-            <h2 class="title-big"><?php the_field('section_lodge_title'); ?></h2>
-            <p class="text-descriptions"><?php the_field('section_lodge_subtitle'); ?></p>
-            <p class="text-body"><?php the_field('section_lodge_description'); ?></p>
+<section id="section-lodge" class="section-lodge bg-white overflow-hidden">
+    <div class="grid grid-cols-2 lg:grid-cols-12 gap-6 mx-14 py-12 border-t-2 border-b-2 border-black lg:py-32">
+        <div class="col-span-6 col-start-2 grid grid-col-6 gap-6">
+            <div class="col-span-6">
+                <h2 class="title-big"><?php the_field('section_lodge_title'); ?></h2>
+            </div>
+            <div class="col-span-1">
+                <div class="w-full max-w-[120px] border-t-2 inline-block border-black mt-4 mr-5"></div>
+            </div>
+            <div class="col-span-5 col-start-2">
+                <p class="text-descriptions"><?php the_field('section_lodge_subtitle'); ?></p>
+                <p class="text-body"><?php the_field('section_lodge_description'); ?></p>
+            </div>
         </div>
-        <div class="col-span-1">
+        <div class="col-span-1 lg:col-span-3 lg:col-start-9">
             <?php 
             $image = get_field('section_lodge_image');
             if( $image ) {
--- template-parts/pages/home/previews.php
+++ template-parts/pages/home/previews.php
@@ -1,8 +1,8 @@
 <section id="section-previews" class="section-previews py-2 lg:pb-32 lg:pt-0 bg-white overflow-hidden">
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-6 px-14">
+    <div class="grid grid-cols-1 lg:grid-cols-12 gap-6 mx-14">
         <h1 class="title-big"><?php the_field('section_previews_main_title_1'); ?></h1>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-6 px-14 py-8">
+    <div class="grid grid-cols-1 lg:grid-cols-12 gap-6 mx-14 py-8">
         <div class="col-span-1 lg:col-start-8">
             <div class="w-full max-w-[120px] border-t-2 inline-block border-black mt-4 mr-5"></div>
         </div>
@@ -10,8 +10,8 @@
             <h2 class="title-subtitle"><?php the_field('section_previews_subtitle_1'); ?></h2>
         </div>
     </div>
-    <div class="container mx-auto grid grid-cols-2 lg:grid-cols-12 pt-8 pb-52">
-        <div class="col-span-1 lg:col-span-7">
+    <div class="grid grid-cols-2 lg:grid-cols-12 gap-6 mx-14 pt-8 pb-52">
+        <div class="col-span-1 lg:col-span-6 lg:col-start-2">
             <?php 
             $preview_image_1 = get_field('section_previews_preview_image_1');
             if( $preview_image_1 ) {
@@ -19,7 +19,7 @@
             }
             ?>
         </div>
-        <div class="col-span-1 lg:col-span-5 flex justify-center items-center flex-col">
+        <div class="col-span-1 lg:col-span-3 lg:col-start-9 flex justify-center items-center flex-col">
             <div>
                 <p class="text-body lg:max-w-md"><?php the_field('section_previews_description_1'); ?></p>
                 <a href="<?php the_field('section_previews_button_link_1'); ?>" class="btn-moreinfo text-button !leading-9"><span class="pt-0"><?php esc_html_e( 'Mehr Erfahren', 'felixlodge' ) ?></span></a>
@@ -27,21 +27,27 @@
         </div>
     </div>
 
-    <div class="lg:max-w-[94%] mx-auto flex justify-end">
-        <h1 class="title-big"><?php the_field('section_previews_main_title_2'); ?></h1>
+    <div class="grid grid-cols-1 lg:grid-cols-12 gap-6 mx-14">
+        <div class="col-span-1 lg:col-span-9 lg:col-start-3 text-right">
+            <h1 class="title-big"><?php the_field('section_previews_main_title_2'); ?></h1>
+        </div>
     </div>
-    <div class="container mx-auto py-8 flex">
-        <div class="w-full max-w-[120px] border-t-2 inline-block border-black mt-4 mr-5"></div>
-        <h2 class="title-subtitle lg:max-w-xl"><?php the_field('section_previews_subtitle_2'); ?></h2>
+    <div class="grid grid-cols-1 lg:grid-cols-12 gap-6 mx-14 py-8">
+        <div class="col-span-1">
+            <div class="w-full max-w-[120px] border-t-2 inline-block border-black mt-4 mr-5"></div>
+        </div>
+        <div class="col-span-1 lg:col-span-4 lg:col-start-2">
+            <h2 class="title-subtitle lg:max-w-xl"><?php the_field('section_previews_subtitle_2'); ?></h2>
+        </div>
     </div>
-    <div class="container mx-auto grid grid-cols-2 lg:grid-cols-12 py-8">
-        <div class="col-span-1 lg:col-span-5 flex justify-center items-center flex-col">
+    <div class="grid grid-cols-2 lg:grid-cols-12 gap-6 mx-14 py-8">
+        <div class="col-span-1 lg:col-span-3 lg:col-start-2 flex justify-center items-center flex-col">
             <div>
                 <p class="text-body lg:max-w-md"><?php the_field('section_previews_description_2'); ?></p>
                 <a href="<?php the_field('section_previews_button_link_2'); ?>" class="btn-moreinfo text-button !leading-9"><span class="pt-0"><?php esc_html_e( 'Mehr Erfahren', 'felixlodge' ) ?></span></a>
             </div>
         </div>
-        <div class="col-span-1 lg:col-span-7">
+        <div class="col-span-1 lg:col-span-6 lg:col-start-6">
             <?php 
             $preview_image_2 = get_field('section_previews_preview_image_2');
             if( $preview_image_2 ) {
