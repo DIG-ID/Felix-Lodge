09eec438ae74c93099145b524fb7700e51047772
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-08-22 11:00:44 +0100

    up

--- template-parts/pages/sleeping/intro.php
+++ template-parts/pages/sleeping/intro.php
@@ -1,5 +1,5 @@
-<section id="section-intro" class="section-intro px-14 pt-72 pb-36 bg-white overflow-hidden">
-	<div class="grid grid-cols-1 lg:grid-cols-12 gap-6 pb-9">
+<section id="section-intro" class="section-intro px-14 pt-72 pb-40 bg-white overflow-hidden">
+	<div class="grid grid-cols-1 lg:grid-cols-12 gap-6 pb-10">
 		<div class="col-span-1 lg:col-span-6">
 			<h1 class="title-big"><?php the_field( 'section_intro_title' ); ?></h1>
 		</div>
--- template-parts/pages/sleeping/paun.php
+++ template-parts/pages/sleeping/paun.php
@@ -1,12 +1,12 @@
 <section id="section-paun" class="section-paun px-14 bg-white overflow-hidden">
-	<div class="grid grid-cols-1 lg:grid-cols-12 py-36">
+	<div class="grid grid-cols-1 lg:grid-cols-12 py-40">
 		<div class=" lg:col-start-2 lg:col-span-5">
 			<h2 class="title-big mb-16">Serneus <br>Für Daheim</h2>
 			<div class="grid grid-cols-5 lg:gap-x-7">
 				<div class="col-span-1">
 					<div class="w-full max-w-[120px] border-b-2 inline-block border-black mt-3"></div>
 				</div>
-				<p class="section-descriptions col-span-4 mb-14">In Paun, unsererem Pop up Store, finden Sie ein kleines, aber feines Sortiment an regionalen Produkten.</p>
+				<p class="section-descriptions col-span-4 mb-10">In Paun, unsererem Pop up Store, finden Sie ein kleines, aber feines Sortiment an regionalen Produkten.</p>
 				<a href="<?php the_permalink(); ?>" class="btn-primary col-start-2"><?php esc_html_e( 'Zum Sortiment', 'felixlodge' ); ?></a>
 			</div>
 		</div>
