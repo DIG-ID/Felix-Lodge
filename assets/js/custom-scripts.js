8c696a96910151cfacacf750d249c40ce7ba8f23
Daniel-Digid <daniel@dig.id>
2023-08-23 16:44:25 +0100

    dev

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function($) {
 
+    
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
+    const mobileMenu = document.querySelector(".mobile-menu");
+
+    mobileMenuToggle.addEventListener("click", function () {
+        mobileMenu.classList.toggle("active");
+    });
 });
--- template-parts/header-main.php
+++ template-parts/header-main.php
@@ -1,7 +1,7 @@
 <header id="header-main" class="header-main w-full top-0 box-border bg-white fixed z-50 px-5 lg:px-16 xl:px-14" itemscope itemtype="http://schema.org/WebSite">
 	<nav class="navigation-main gap-5 lg:gap-4 xl:gap-8 grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12" role="navigation" aria-label="<?php esc_attr_e( 'Main navigation', 'paun' ); ?>">
-		<div class="navigation-main-content flex flex-col sm:flex-row col-span-1 lg:col-span-12">
-			<div class="w-full mx-auto flex flex-wrap items-center justify-center px-0 py-6 sm:py-12">
+		<div class="navigation-main-content flex flex-col sm:flex-row col-span-4 md:col-span-8 xl:col-span-12">
+			<div class="w-full mx-auto flex flex-wrap items-center justify-center px-0 py-12">
 				<div class="header-logo-wrapper absolute mb-4 sm:mb-0">
 					<?php 
 					$main_logo = get_field('settings_website_logo', 'option');
@@ -13,6 +13,9 @@
 				</div>
 
 				<div class="w-full">
+					<button id="mobile-menu-toggle" class="mobile-menu-toggle float-right">
+						<span class="menu-icon"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/burger-menu.svg" title="mobile menu" alt="mobile menu"></span>
+					</button>
 					<?php
 					wp_nav_menu(
 						array(
@@ -26,10 +29,22 @@
 							'after'          => '', // Add any necessary HTML after the menu item
 						)
 					);
-					
 					?>
 				</div>
 			</div>
 		</div>
 	</nav>
+	<div class="mobile-menu">
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'main',
+				'container'      => false,
+				'menu_class'     => 'main-menu-responsive block text-main-menu uppercase', // Adjust classes here
+				'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'fallback_cb'    => '__return_false',
+			)
+		);
+		?>
+	</div>
 </header>
