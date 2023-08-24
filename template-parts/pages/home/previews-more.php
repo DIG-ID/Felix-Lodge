7b54f88940536182fbd2f919d438eb9737ecd6eb
Daniel-Digid <daniel@dig.id>
2023-08-24 11:37:01 +0100

    d

--- template-parts/pages/home/previews-more.php
+++ template-parts/pages/home/previews-more.php
@@ -1,9 +1,9 @@
 <section id="section-previews-more" class="section-previews-more px-5 lg:px-16 xl:px-14 bg-white overflow-hidden">
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 pt-40">
-        <h2 class="title-big col-span-1 lg:col-span-7 lg:col-start-3"><?php the_field('previews_more_title'); ?></h2>
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 pt-20 xl:pt-40">
+        <h2 class="title-big col-span-4 md:col-span-8 xl:col-span-7 xl:col-start-3"><?php the_field('previews_more_title'); ?></h2>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 py-40">
-        <div class="col-span-1 lg:col-span-3 lg:col-start-2">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 py-20 xl:py-40">
+        <div class="col-span-3 md:col-span-3 xl:col-span-3 xl:col-start-2 hidden md:block">
             <?php 
             $preview_image_1 = get_field('previews_more_image_1');
             if( $preview_image_1 ) {
@@ -11,16 +11,16 @@
             }
             ?>
         </div>
-        <div class="col-span-1 lg:col-start-8">
+        <div class="col-span-1 col-start-2 md:col-start-5 xl:col-start-8">
             <div class="w-full max-w-[120px] border-b-2 inline-block border-black mt-6"></div>
         </div>
-        <div class="col-span-1 lg:col-span-3 lg:col-start-9">
+        <div class="col-span-2 md:col-span-3 xl:col-span-3 col-start-3 md:col-start-6 xl:col-start-9">
             <p class="title lg:max-w-xl"><?php the_field('previews_more_subtitle'); ?></p>
         </div>
-        <div class="col-span-1 lg:col-span-3 lg:col-start-3">
-            <p class="text-body pt-44"><?php the_field('previews_more_description'); ?></p>
+        <div class="col-span-3 md:col-span-3 xl:col-span-3 xl:col-start-3">
+            <p class="text-body pt-10 md:pt-44"><?php the_field('previews_more_description'); ?></p>
         </div>
-        <div class="col-span-1 lg:col-span-4 lg:col-start-7 -mt-10">
+        <div class="col-span-3 md:col-span-4 xl:col-span-4 col-start-2 md:col-start-5 xl:col-start-7 mt-10 xl:-mt-10">
             <?php 
             $preview_image_1 = get_field('previews_more_image_2');
             if( $preview_image_1 ) {
@@ -29,11 +29,11 @@
             ?>
         </div>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 pt-40">
-        <h2 class="title-big col-span-1 lg:col-span-6 lg:col-start-5 text-right"><?php the_field('previews_more_title_2'); ?></h2>
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 pt-20 xl:pt-40">
+        <h2 class="title-big col-span-4 md:col-span-7 xl:col-span-6 xl:col-start-5 xl:text-right pb-20 md:pb-40 xl:pb-0"><?php the_field('previews_more_title_2'); ?></h2>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 pb-40">
-        <div class="col-span-1 lg:col-span-4 lg:col-start-2">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 pb-40">
+        <div class="col-span-3 md:col-span-4 xl:col-span-4 xl:col-start-2">
             <?php 
             $preview_image_1 = get_field('previews_more_image_3');
             if( $preview_image_1 ) {
@@ -41,16 +41,16 @@
             }
             ?>
         </div>
-        <div class="col-span-1 lg:col-start-8 flex flex-col justify-center">
+        <div class="col-span-1 col-start-2 md:col-start-6 xl:col-start-8 flex flex-col justify-center">
             <div class="w-full max-w-[120px] border-b-2 inline-block border-black"></div>
         </div>
-        <div class="col-span-1 lg:col-span-3 lg:col-start-9 flex flex-col justify-center">
+        <div class="col-span-2 md:col-span-2 xl:col-span-3 col-start-3 md:col-start-7 xl:col-start-9 flex flex-col justify-center hyphenate">
             <p class="title lg:max-w-xl"><?php the_field('previews_more_subtitle_2'); ?></p>
         </div>
-        <div class="col-span-1 lg:col-span-3 lg:col-start-4">
+        <div class="col-span-3 md:col-span-3 xl:col-span-3 md:col-start-2 xl:col-start-4">
             <p class="text-body pt-44"><?php the_field('previews_more_description_2'); ?></p>
         </div>
-        <div class="col-span-1 lg:col-span-3 lg:col-start-9 -mt-10">
+        <div class="col-span-2 md:col-span-3 xl:col-span-3 col-start-3 md:col-start-6 xl:col-start-9 -mt-10">
             <?php 
             $preview_image_1 = get_field('previews_more_image_4');
             if( $preview_image_1 ) {
