8332fe0f166e1f86ada32828506f6e8a4aa482f7
Daniel-Digid <daniel@dig.id>
2023-08-24 10:07:21 +0100

    d

--- template-parts/pages/home/intro.php
+++ template-parts/pages/home/intro.php
@@ -1,6 +1,6 @@
 <section id="section-intro" class="section-intro lg:pb-32 pt-0 px-5 lg:px-16 xl:px-14 bg-white overflow-hidden">
-    <div class="grid grid-cols-1 lg:grid-cols-12 pt-32">
-        <div class="col-span-1 lg:col-span-12">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 pt-32">
+        <div class="col-span-4 md:col-span-8 xl:col-span-12">
         <?php 
         $header_image = get_field('section_intro_header_image');
         if( $header_image ) {
@@ -9,47 +9,47 @@
         ?>
         </div>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 py-8">
-        <div class="col-span-1 lg:col-span-6">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 py-8">
+        <div class="col-span-4 md:col-span-8 xl:col-span-6">
             <h1 class="title-big"><?php the_field('section_intro_first_title'); ?></h1>
         </div>
-        <div class="col-span-1">
+        <div class="col-span-1 col-start-2 xl:col-start-7">
             <div class="w-full max-w-[120px] border-b-2 inline-block border-black mt-3"></div>
         </div>
-        <div class="col-span-1 lg:col-span-2">
+        <div class="col-span-3 md:col-span-2 xl:col-span-2">
             <div class="col-span-2 text-body"><?php the_field('section_intro_text_box_1'); ?></div>
         </div>
-        <div class="col-span-1 lg:col-span-3 text-right">
+        <div class="col-span-4 md:col-span-4 xl:col-span-3 col-start-1 md:col-start-5 xl:col-start-10 text-right">
             <a href="<?php the_permalink(); ?>" class="btn-primary"><?php esc_html_e( 'Jetzt Buchen', 'felixlodge' ) ?></a>
         </div>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 py-8">
-        <div class="col-span-1 lg:col-span-7 lg:col-start-6 text-right">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 py-8">
+        <div class="col-span-4 md:col-span-5 xl:col-span-7 md:col-start-4 xl:col-start-6 text-right">
             <h2 class="title-big"><?php the_field('section_intro_second_title'); ?></h1>
         </div>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 py-8">
-        <div class="col-span-1 lg:col-start-2">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 pt-8 xl:py-8">
+        <div class="col-span-1 xl:col-start-2">
             <div class="w-full max-w-[120px] border-b-2 inline-block border-black mt-3"></div>
         </div>
-        <div class="col-span-1 lg:col-span-2">
+        <div class="col-span-3 md:col-span-2 xl:col-span-2">
             <div class="col-span-2 text-body lg:max-w-[270px]"><?php the_field('section_intro_text_box_2'); ?></div>
         </div>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 py-8">
-        <div class="col-span-1 lg:col-start-9">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 xl:py-8">
+        <div class="col-span-1 md:col-start-6 xl:col-start-9">
             <div class="w-full max-w-[120px] border-b-2 inline-block border-black mt-3"></div>
         </div>
-        <div class="col-span-1 lg:col-span-2 lg:col-start-10">
+        <div class="col-span-3 md:col-span-2 xl:col-span-2 md:col-start-7 xl:col-start-10">
             <div class="col-span-2 text-body lg:max-w-[270px]"><?php the_field('section_intro_text_box_3'); ?></div>
         </div>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 ">
-        <div class="col-span-1 lg:col-span-3 lg:col-start-3">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8">
+        <div class="col-span-3 xl:col-span-3 xl:col-start-3">
             <p class="title"><?php the_field('section_intro_intro_image_description'); ?></p>
         </div>
-        <div class="col-span-1 lg:col-span-3 lg:col-start-8">
-            <div class="col-span-2 pt-52">
+        <div class="col-span-2 md:col-span-4 xl:col-span-3 col-start-3 md:col-start-5 xl:col-start-8">
+            <div class="md:pt-28 xl:pt-52">
                 <?php 
                 $intro_image = get_field('section_intro_intro_image');
                 if( $intro_image ) {
--- template-parts/pages/home/previews.php
+++ template-parts/pages/home/previews.php
@@ -1,17 +1,17 @@
 <section id="section-previews" class="section-previews lg:pb-40 lg:pt-0 px-5 lg:px-16 xl:px-14 bg-white overflow-hidden">
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 pt-24 lg:pt-0">
         <h1 class="title-big"><?php the_field('section_previews_main_title_1'); ?></h1>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-x-6 py-8">
-        <div class="col-span-1 lg:col-start-8">
-            <div class="w-full max-w-[120px] border-t-2 inline-block border-black mt-6 mr-5"></div>
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 py-8">
+        <div class="col-span-1 col-start-2 md:col-start-3 xl:col-start-8">
+            <div class="w-full max-w-[120px] border-t-2 inline-block border-black mt-4 xl:mt-6 mr-5"></div>
         </div>
-        <div class="col-span-1 lg:col-span-4 lg:col-start-9">
+        <div class="col-span-2 md:col-span-4 xl:col-span-4 col-start-3 md:col-start-4 xl:col-start-9">
             <h2 class="title"><?php the_field('section_previews_subtitle_1'); ?></h2>
         </div>
     </div>
-    <div class="grid grid-cols-2 lg:grid-cols-12 gap-x-6 pt-8 pb-52">
-        <div class="col-span-1 lg:col-span-6 lg:col-start-2">
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 pt-8 pb-52">
+        <div class="col-span-4 md:col-span-8 lg:col-span-8 xl:col-span-6 xl:col-start-2">
             <?php 
             $preview_image_1 = get_field('section_previews_preview_image_1');
             if( $preview_image_1 ) {
@@ -19,10 +19,10 @@
             }
             ?>
         </div>
-        <div class="col-span-1 lg:col-span-3 lg:col-start-9 flex justify-center items-center flex-col">
-            <div>
-                <p class="text-body lg:max-w-md !mb-16"><?php the_field('section_previews_description_1'); ?></p>
-                <a href="<?php the_field('section_previews_button_link_1'); ?>" class="btn-primary text-button !leading-9"><span class="pt-0"><?php esc_html_e( 'Mehr Erfahren', 'felixlodge' ) ?></span></a>
+        <div class="col-span-3 md:col-span-8 xl:col-span-3 col-start-2 md:col-start-1 xl:col-start-9 flex justify-center items-center flex-col">
+            <div class="w-full md:pt-14 xl:pt-0">
+                <p class="text-body md:max-w-md !mb-16 float-none md:float-left xl:float-none"><?php the_field('section_previews_description_1'); ?></p>
+                <a href="<?php the_field('section_previews_button_link_1'); ?>" class="btn-primary float-none md:float-right xl:float-none"><span class="pt-0"><?php esc_html_e( 'Mehr Erfahren', 'felixlodge' ) ?></span></a>
             </div>
         </div>
     </div>
