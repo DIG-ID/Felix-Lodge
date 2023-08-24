c67ef7b8bddb1d5efc4f1634734bdc974ea52154
Daniel-Digid <daniel@dig.id>
2023-08-24 11:10:22 +0100

    update buttons sass

--- template-parts/pages/home/lodge.php
+++ template-parts/pages/home/lodge.php
@@ -1,7 +1,7 @@
 <section id="section-lodge" class="section-lodge px-5 lg:px-16 xl:px-14 bg-white overflow-hidden">
 <hr class="border-t-2 border-black">
     <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 py-20 xl:py-40">
-		<div class="col-span-3 md:col-span-5 xl:col-span-5 xl:col-start-2">
+		<div class="col-span-3 md:col-span-5 xl:col-span-5 xl:col-start-2 order-2 md:order-1">
 			<h2 class="title-big mb-16"><?php the_field('section_lodge_title'); ?></h2>
 			<div class="grid grid-cols-4 md:grid-cols-5 gap-5 lg:gap-4 xl:gap-8">
 				<div class="col-span-1">
@@ -12,7 +12,7 @@
 				<a href="<?php the_permalink(); ?>" class="btn-primary col-start-2"><?php esc_html_e( 'Zum Sortiment', 'felixlodge' ); ?></a>
 			</div>
 		</div>
-		<div class="col-span-2 md:col-span-3 xl:col-span-3 md:col-start-6 xl:col-start-8"> 
+		<div class="col-span-2 md:col-span-3 xl:col-span-3 md:col-start-6 xl:col-start-8 order-1 md:order-2 pb-8 md:pb-0"> 
 			<?php
 			$image = get_field( 'section_lodge_image' );
 			if ( $image ) :
