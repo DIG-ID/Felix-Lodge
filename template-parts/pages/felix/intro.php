a1c1beb4f41276744f8c671764fd898110ac70bf
Daniel-Digid <daniel@dig.id>
2023-08-30 11:05:52 +0100

    d

--- template-parts/pages/felix/intro.php
+++ template-parts/pages/felix/intro.php
@@ -1,16 +1,26 @@
-<section id="section-intro" class="section-intro px-5 lg:px-16 xl:px-14 pt-72 pb-40 bg-white overflow-hidden">
-	<div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-8 xl:grid-cols-12 gap-5 lg:gap-4 xl:gap-8 pb-10">
-		<div class="col-span-4 md:col-span-4 lg:col-span-4 xl:col-span-4">
-			<h1 class="title-big mb-16 lg:mb-0"><?php the_field( 'section_intro_title' ); ?></h1>
+<section id="section-intro" class="section-intro pt-16 md:pt-36 lg:pt-72 md:pb-0 container-margins bg-white overflow-hidden">
+	<div class="container-grid pt-28">
+		<div class="col-span-4 md:col-span-3 lg:col-span-4 xl:col-span-4">
+			<h1 class="title-big mb-16 lg:mb-0 hyphenate"><?php the_field( 'section_intro_title' ); ?></h1>
 		</div>
-		<div class="col-span-1 xl:col-start-8">
+		<div class="col-span-1 xl:col-start-7">
 			<div class="w-full max-w-[120px] border-b-2 inline-block border-black mt-3"></div>
 		</div>
-		<div class="col-span-3 md:col-span-3 lg:col-span-3 xl:col-span-3">
-			<div class="col-span-2 text-body"><?php the_field( 'section_intro_description' ); ?></div>
+		<div class="col-span-3 md:col-span-3 lg:col-span-3 xl:col-span-3 xl:col-start-8">
+			<div class="col-span-2 text-body"><?php the_field( 'section_intro_description_1' ); ?></div>
 		</div>
 	</div>
-	<div class="text-center">
-		<a href="<?php the_permalink(); ?>" class="btn-moreinfo text-button"><span class="pt-1"><?php esc_html_e( 'Jetzt Buchen', 'felixlodge' ); ?></span></a>
+	<div class="container-grid">
+		<div class="col-span-4 md:col-span-4 xl:col-span-3 xl:col-start-3 items-center flex order-2 xl:order-1">
+			<h1 class="text-body"><?php the_field( 'section_intro_description_2' ); ?></h1>
+		</div>
+		<div class="col-span-4 md:col-span-8 xl:col-span-6 xl:col-start-7 order-1 xl:order-2 pt-14 md:pt-36 xl:pt-0 pb-10 md:pb-12 xl:pb-0">
+			<?php 
+			$intro_image = get_field('section_intro_image');
+			if( $intro_image ) {
+				echo wp_get_attachment_image( $intro_image, 'full' );
+			}
+			?>
+		</div>
 	</div>
 </section>
---
+++ template-parts/pages/felix/team.php
@@ -0,0 +1,28 @@
+<section id="section-team" class="section-team pt-16 md:pt-24 xl:pt-72 pb-28 md:pb-36 xl:pb-40 container-margins bg-white overflow-hidden">
+    <div class="container-grid">
+        <div class="col-span-4 md:col-span-8 xl:col-span-12">
+            <h2 class="title-big mb-16 xl:mb-[90px]"><?php the_field( 'team_title' ); ?></h2>
+        </div>
+    </div>
+    <div class="container-grid">
+    <?php
+    if( have_rows('team_list') ):
+    while( have_rows('team_list') ) : the_row(); ?>
+        <div class="col-span-3 md:col-span-4 xl:col-span-4">
+            <?php 
+			$image = get_sub_field('image');
+			if( $image ) {
+				echo wp_get_attachment_image( $image, 'team-image', false, array('class' => 'w-full'));
+			}
+            else{
+                echo '<img src="https://placehold.co/574x580">';
+            }
+			?>
+            <div class="section-descriptions py-10"><?php the_sub_field( 'name' ); ?></div>
+            <div class="text-body"><?php the_sub_field( 'description' ); ?></div>
+        </div>
+    <?php 
+    endwhile;
+    endif; ?>
+    </div>
+</section>
