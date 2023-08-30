821077427a21669520f07b328f1ad335c4901423
Daniel-Digid <daniel@dig.id>
2023-08-30 14:49:06 +0100

    d

--- header.php
+++ header.php
@@ -1,2 +1,3 @@
 <?php get_template_part( 'template-parts/header' ); ?>
 <?php get_template_part( 'template-parts/header', 'main' ); ?>
+<?php get_template_part( 'template-parts/components/booking-bar' ); ?>
---
+++ template-parts/components/booking-bar.php
@@ -0,0 +1,3 @@
+<div class="fixed w-full bg-white footbar-with-shadow bottom-0 left-0 flex justify-center py-[19px]">
+    <a href="<?php the_field( 'settings_simple_booking_url', 'options') ?>" target="_blank" class="btn-primary"><?php esc_html_e( 'Jetzt Buchen', 'felixlodge' ) ?></a>
+</div>
