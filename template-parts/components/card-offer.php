85ffc15299a408de921eb5e27e920a1f601c15a9
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-08-31 12:41:17 +0100

    up

--- template-parts/components/card-offer.php
+++ template-parts/components/card-offer.php
@@ -5,9 +5,9 @@
 	<div class="col-span-4 md:col-span-6 xl:col-span-5 flex flex-col justify-between">
 		<div class="card-offer-wrapper">
 			<?php the_title( '<h3 class="font-sans text-base md:text-xl xl:text-3xl text-black leading-7 tracking-[0.56px] line-clamp-2 mb-10">', '</h3>' ); ?>
-			<div class="card-offer-content md:mb-10 xl:mb-20 overflow-hidden"><?php echo wp_kses_post( get_the_content() ); ?></div>
+			<div class="card-offer-content mb-6 md:mb-10 xl:mb-20 overflow-hidden"><?php echo wp_kses_post( get_the_content() ); ?></div>
 		</div>
-		<button class="btn-offer-readmore relative z-10 flex items-center justify-center text-center md:hidden md:invisible mb-10 pt-1 sm:pt-6 ">
+		<button class="btn-offer-readmore relative z-10 flex items-center justify-center text-center md:hidden md:invisible mb-10 pt-1 sm:pt-6">
 			<span class="font-sans uppercase text-sm leading-6"><?php esc_html_e( 'Read more', 'felixlodge' ); ?></span>
 			<svg xmlns="http://www.w3.org/2000/svg" width="13" height="7" viewBox="0 0 13 7" fill="none" class="ml-1 mb-1">
 				<path d="M12 1L6.5 6L1 1" stroke="black"/>
