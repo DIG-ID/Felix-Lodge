c4e2c731b4e289e4ce31b39e413275f124c0bff5
Daniel-Digid <daniel@dig.id>
2023-09-01 11:05:49 +0100

    d

--- template-parts/pages/page/page-content.php
+++ template-parts/pages/page/page-content.php
@@ -1,5 +1,5 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'pt-16 md:pt-36 lg:pt-72 md:pb-36 container-margins bg-white overflow-hidden' ); ?>>
-	<div class="container mx-auto px-8 xl:px-0 grid grid-cols-1 lg:grid-cols-12">
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'pt-36 md:pt-36 lg:pt-72 md:pb-36 container-margins bg-white overflow-hidden' ); ?>>
+	<div class="container mx-auto px-0 grid grid-cols-1 lg:grid-cols-12">
 		<div class="post-content-wrapper col-span-8">
 			<h1 class="title-big"><?php the_title(); ?></h1>
 			
