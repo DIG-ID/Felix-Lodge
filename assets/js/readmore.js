1759f211e48f84600ad16d79e8cb1aa8e22f982a
Daniel-Digid <daniel@dig.id>
2023-12-18 17:37:53 +0000

    d

--- assets/js/readmore.js
+++ assets/js/readmore.js
@@ -6,20 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
     if (document.body.classList.contains("page-template-page-offers")) {
 
       $(function() {
-
-
-    
         $('.btn-offer-readmore').on( 'click', (e) => {
           e.preventDefault();
           $('.card-offer-content').toggleClass('open');
           $('.btn-offer-readmore').toggleClass('open');
+          // Check if the content is open or closed
+          const isOpen = $('.card-offer-content').hasClass('open');
+
+          // Update the button text based on the content state
+          const buttonText = isOpen ? 'Read less' : 'Read more';
+          $('.btn-offer-readmore span').text(buttonText);
         });
-    
-      
     });
 
     }
-
-
   }, false);
 });
